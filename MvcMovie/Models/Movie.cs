189a9e0869d06da5e0790d8ac97db774f65a35b4
Adam Terebiński <aterebinski@wp.pl>
2021-10-21 06:47:36 +0100

    Annotations

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,9 +11,11 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        [DataType(DataType.DateTime)]
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
